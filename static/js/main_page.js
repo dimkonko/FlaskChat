321b84d2d05d1ba561722b38f65625f35370eb94
Dima <d.karacheban@gmail.com>
2014-09-09 22:00:20 +0300

    Changed main page template

--- static/js/main_page.js
+++ static/js/main_page.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-	var secondLine = $("#secondLine");
-
 	$("#firstLine").fadeTo(1000, 1, function() {
-		secondLine.fadeTo(1000, 1, function() {
-			$("#logButtons").fadeTo(1000, 1);
+		$("#secondLine").fadeTo(1000, 1, function() {
+			$("#menubar").fadeTo(1000, 1);
 		})
 	});
 }
